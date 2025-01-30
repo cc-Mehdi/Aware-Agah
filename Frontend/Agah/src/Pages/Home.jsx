5cfb0d4eac595d2475f30dab6b89efc030d16537
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-30 09:37:23 +0330

    :sparkles: Add set price component to home page

--- Frontend/Agah/src/Pages/Home.jsx
+++ Frontend/Agah/src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ProductStatusList from './../components/product_status_card_list'
+import SetPriceAlertBox from './../components/set_price_alert_box'
 
 const Home = () => {
     return (
-        <ProductStatusList />
+        <>
+            <ProductStatusList />
+            <SetPriceAlertBox />
+        </>
     )
 }
 
