b0eea78371b1b68f0473273256cf36a418eb054f
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-31 01:50:17 +0330

    :sparkles: Add new component for select "Alert Platform"

---
+++ Frontend/Agah/src/components/radiobutton_platform_selector.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const RadioButtonPlatformSelector = ({ platforms }) => {
+    return (
+        <>
+            <ul className="grid w-full gap-6 md:grid-cols-2">
+                {platforms.map((platform, index) => (
+                    <li key={index}>
+                        <input type="radio" id={platform.id} name="hosting" value={platform.id} className="hidden peer" required />
+                        <label
+                            htmlFor={platform.id}
+                            className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                        >
+                            <div className="block">
+                                <div className="w-full text-lg font-semibold">{platform.title}</div>
+                                <div className="w-full">{platform.description}</div>
+                            </div>
+                            <svg className="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </label>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default RadioButtonPlatformSelector;
