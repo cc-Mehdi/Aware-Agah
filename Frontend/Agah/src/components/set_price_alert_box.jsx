307ad735821d43ee0a1839a025c9fa2890f218d7
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-31 01:54:40 +0330

    :construction: Change main page for Get product names from api and show into product dropdown

--- Frontend/Agah/src/components/set_price_alert_box.jsx
+++ Frontend/Agah/src/components/set_price_alert_box.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState, useEffect, useRef } from 'react'
 import Dropdown from './dropdown'
 import NumericalInput from './numerical_input'
 import { sendPriceAlert } from './../services/api_SetPriceAlert';
@@ -6,6 +6,31 @@ import { getProductNames } from './../services/api_GetProductNames';
 
 const set_price_alert_box = () => {
 
+    const [productList, setProductList] = useState([]);
+    const isMounted = useRef(false); // for fixing bug (this component send 2 request when loaded and this code fix this send just 1 request)
+
+
+    // Fetch product names when the component mounts
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await getProductNames();
+
+                // Ensure we correctly parse the inner JSON
+                const parsedData = JSON.parse(response.result);
+
+                setProductList(parsedData);
+            } catch (error) {
+                console.error("Failed to fetch product names:", error);
+                setProductList(["خطا در دریافت داده‌ها"]);
+            }
+        };
+        if (!isMounted.current) {
+            fetchProducts(); // Fetch only once
+            isMounted.current = true;
+        }
+
+    }, []);
     const handleSendAlert = async () => {
         try {
             const response = await sendPriceAlert({
@@ -27,7 +52,7 @@ const set_price_alert_box = () => {
             <div className="w-full p-6 py-18 flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
 
                 <div className='w-full my-2'>
-                    <Dropdown inputName={"Product"} label={"محصول را انتخاب کنید"} />
+                    <Dropdown inputName={"Product"} label={"محصول را انتخاب کنید"} dataSource={productList} />
                 </div>
                 <div className='w-full flex my-2'>
                     <NumericalInput inputName={"MinPrice"} label={"حداقل"} />
