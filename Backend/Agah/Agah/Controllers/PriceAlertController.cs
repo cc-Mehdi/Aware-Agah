45c2ad732680a1dbb99b8d6ce4765391d287a750
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-30 09:30:54 +0330

    :construction: Add two Endpoint for api's

---
+++ Backend/Agah/Agah/Controllers/PriceAlertController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace Agah.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriceAlertController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SetPriceAlert([FromBody] PriceAlertRequest request)
+        {
+            var userSelectedRange = new PriceAlertRequest
+            {
+                UserId = request.UserId,
+                Product = request.Product,
+                MinPrice = request.MinPrice,
+                MaxPrice = request.MaxPrice,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            return Ok(new { message = "Price alert set successfully!" });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductNames()
+        {
+            // Create the list
+            List<string> items = new List<string>
+        {
+            "گرم طلای 18 عیار",
+            "سکه بهار آزادی"
+        };
+
+            // Convert the list to JSON
+            string json = JsonConvert.SerializeObject(items);
+
+
+            return Ok(new { result = json });
+        }
+    }
+
+    public class PriceAlertRequest
+    {
+        public int UserId { get; set; }
+        public string Product { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
