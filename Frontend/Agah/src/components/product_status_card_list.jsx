fb1c5318f558606d1d12f2b74ca44291acc5da04
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-31 17:26:30 +0330

    :lipstick: Add hint for products box in main page

--- Frontend/Agah/src/components/product_status_card_list.jsx
+++ Frontend/Agah/src/components/product_status_card_list.jsx
@@ -29,22 +29,32 @@ const ProductStatusCardList = () => {
     }, []);
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-            {goldData.length > 0 ? (
-                goldData.map((item, index) => (
-                    <ProductStatusCard
-                        key={index}
-                        name={item.name}
-                        amount={item.price}
-                        unit={item.unit}
-                        changedPercent="0%"
-                        changedAmount="0"
-                    />
-                ))
-            ) : (
-                <p className="text-white">Loading...</p>
-            )}
-        </div>
+        <>
+            <div className='bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 my-5 px-6 py-14'>
+
+                <div className='mb-10'>
+                    <h5 className="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">قیمت محصولات</h5>
+                    <span className="font-bold text-lg text-gray-500">لیست قیمت محصولات مرتبط با طلا</span>
+                </div>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {goldData.length > 0 ? (
+                        goldData.map((item, index) => (
+                            <ProductStatusCard
+                                key={index}
+                                name={item.name}
+                                amount={item.price}
+                                unit={item.unit}
+                                changedPercent="0%"
+                                changedAmount="0"
+                            />
+                        ))
+                    ) : (
+                        <p className="text-white">Loading...</p>
+                    )}
+                </div>
+            </div>
+        </>
     );
 };
 
