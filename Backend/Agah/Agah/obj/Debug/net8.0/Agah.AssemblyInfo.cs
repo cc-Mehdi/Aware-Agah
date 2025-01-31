58479fe42e99a66091474e2e7819d17b70dbfff8
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-31 17:39:41 +0330

    🚧 VS updated some files

--- Backend/Agah/Agah/obj/Debug/net8.0/Agah.AssemblyInfo.cs
+++ Backend/Agah/Agah/obj/Debug/net8.0/Agah.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agah")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b3c287581e4c49046be50d1a43f2d4fde17664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf75a6acf409455919dd88424244cd1735e6734e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agah")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agah")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
