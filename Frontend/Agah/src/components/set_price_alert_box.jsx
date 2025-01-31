53eb39b2a281d46d0788e080b0cbbb95da27df24
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-31 17:27:59 +0330

    :lipstick: Add hint for SetPriceAlert box

--- Frontend/Agah/src/components/set_price_alert_box.jsx
+++ Frontend/Agah/src/components/set_price_alert_box.jsx
@@ -9,6 +9,7 @@ const set_price_alert_box = () => {
 
     const [productList, setProductList] = useState([]);
     const isMounted = useRef(false); // for fixing bug (this component send 2 request when loaded and this code fix this send just 1 request)
+    const [selectedPlatform, setSelectedPlatform] = useState();
 
 
     // Fetch product names when the component mounts
@@ -32,6 +33,7 @@ const set_price_alert_box = () => {
         }
 
     }, []);
+
     const handleSendAlert = async () => {
         try {
             const response = await sendPriceAlert({
@@ -39,6 +41,7 @@ const set_price_alert_box = () => {
                 product: document.querySelector("#Product").value,
                 minPrice: document.querySelector("#MinPrice").value,
                 maxPrice: document.querySelector("#MaxPrice").value,
+                platform: selectedPlatform,
                 createdAt: "2025-01-29T19:11:58.386Z"
             });
 
@@ -50,7 +53,12 @@ const set_price_alert_box = () => {
 
     return (
         <>
-            <div className="w-full p-6 py-18 flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-full p-6 py-14 flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+
+                <div className='w-full mb-10'>
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">انتخاب بازه قیمت</h5>
+                    <span className="font-bold text-lg text-gray-500">برای محصول مورد نظر بازه قیمتی مشخص کنید تا در صورت خارج شدن از بازه به شما هشدار داده شود</span>
+                </div>
 
                 <div className='w-full my-2'>
                     <Dropdown inputName={"Product"} label={"محصول را انتخاب کنید"} dataSource={productList} />
@@ -60,12 +68,14 @@ const set_price_alert_box = () => {
                     <NumericalInput inputName={"MaxPrice"} label={"حداکثر"} />
                 </div>
                 <div className='w-full my-2'>
-                    <PlatformSelector platforms={
+                    <PlatformSelector inputName={'Platform'} dataSource={
                         [{ 'id': '1', 'title': 'ایمیل', 'description': 'اطلاع رسانی با ایمیل' },
                         { 'id': '2', 'title': 'تلفن', 'description': 'اطلاع رسانی با تماس تلفنی' },
                         { 'id': '3', 'title': 'پیامک', 'description': 'اطلاع رسانی با پیامک' },
                         { 'id': '4', 'title': 'نوتیف درون برنامه', 'description': 'اطلاع رسانی با نوتیفیکیشن' }]
-                    } />
+                    }
+                        selectedValue={selectedPlatform}
+                        onChange={setSelectedPlatform} />
                 </div>
 
                 <button onClick={handleSendAlert} type="button" className="text-white w-full py-5 mt-7 hover:bg-gray-800 cursor-pointer bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-600 dark:border-gray-700">ثبت</button>
