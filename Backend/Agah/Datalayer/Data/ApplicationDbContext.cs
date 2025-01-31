7f064193ddf09306f919dd6b008a666f9c47fcda
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-01-31 21:39:00 +0330

    :card_file_box: Config "DbContext"

---
+++ Backend/Agah/Datalayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Datalayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datalayer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductLog> ProductLog { get; set; }
+        public DbSet<Alarm> Alarm { get; set; }
+        public DbSet<Reserve> Reserve { get; set; }
+    }
+}
