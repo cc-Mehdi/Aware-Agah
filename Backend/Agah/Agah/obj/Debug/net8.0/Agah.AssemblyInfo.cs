ef2b99617d121e835b383b8f44fc2844fcedf6aa
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-01 23:27:41 +0330

    :construction: VS updated base files

--- Backend/Agah/Agah/obj/Debug/net8.0/Agah.AssemblyInfo.cs
+++ Backend/Agah/Agah/obj/Debug/net8.0/Agah.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agah")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58479fe42e99a66091474e2e7819d17b70dbfff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8177ca2ebe89c32583935112ea9421b794474aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agah")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agah")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Agah/Datalayer/obj/Debug/net8.0/Datalayer.AssemblyInfo.cs
+++ Backend/Agah/Datalayer/obj/Debug/net8.0/Datalayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datalayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58479fe42e99a66091474e2e7819d17b70dbfff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8177ca2ebe89c32583935112ea9421b794474aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datalayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datalayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
