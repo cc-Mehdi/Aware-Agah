7030629d70e2e9988fc5215f07b99a551f3dcf3e
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-01 23:26:18 +0330

    :sparkles: Add unitOfWork files

---
+++ Backend/Agah/Datalayer/Repositories/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Datalayer.Repositories.IRepositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UserRepository { get; set; }
+        IProductRepository ProductRepository {get; set;}
+        IAlarmRepository AlarmRepository {get; set;}
+        IProductLogRepository ProductLogRepository {get; set;}
+        IReserveRepository ReserveRepository {get; set;}
+        void Save();
+    }
+}
---
+++ Backend/Agah/Datalayer/Repositories/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Datalayer.Data;
+using Datalayer.Repositories.IRepositories;
+
+namespace Datalayer.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db) : base()
+        {
+            _db = db;
+            UserRepository = new UserRepository(_db);
+            ProductRepository = new ProductRepository(_db);
+            AlarmRepository = new AlarmRepository(_db);
+            ProductLogRepository = new ProductLogRepository(_db);
+            ReserveRepository = new ReserveRepository(_db);
+        }
+
+        public IUserRepository UserRepository{ get; set; }
+        public IProductRepository ProductRepository { get; set; }
+        public IAlarmRepository AlarmRepository { get; set; }
+        public IProductLogRepository ProductLogRepository { get; set; }
+        public IReserveRepository ReserveRepository { get; set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
