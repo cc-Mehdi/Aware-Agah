f99e96d423d42b7f71d9ad6a42248a193ed7147e
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-05 19:50:27 +0330

    :construction: Frontend | Add api component for getting productlogs from our services

---
+++ Frontend/Agah/src/services/api_GetProductsLog.jsx
@@ -0,0 +1,17 @@
+export const getProductsLog = async () => {
+    try {
+        const response = await fetch('https://localhost:44314/api/Product/GetProductsLog', {
+            method: 'Get',
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
