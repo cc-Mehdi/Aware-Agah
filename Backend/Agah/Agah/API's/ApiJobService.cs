47f053fdf5cb034bf74d4b3e5d40e8611bf6651a
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-06 22:34:40 +0330

    :construction: Add a class for call some apis for job services

---
+++ Backend/Agah/Agah/API's/ApiJobService.cs
@@ -0,0 +1,34 @@
+﻿using Datalayer.Repositories.IRepositories;
+
+namespace Agah.API_s
+{
+    public static class ApiJobService
+    {
+        public static async Task CallUpdateProductLogApi()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = "https://localhost:44314/api/Product/UpdateProductLog";
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"UpdateProductLog API Response: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error calling UpdateProductLog API: {ex.Message}");
+                }
+            }
+        }
+
+        public static async Task CallCheckPriceInReservedsApi(int userId)
+        {
+
+        }
+    }
+
+}
