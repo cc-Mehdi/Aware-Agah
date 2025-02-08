b48392c4136f4ef12cacf433d1da82c00e1756bf
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-08 20:57:22 +0330

    :white_check_mark: Backend | Add "CallCheckPriceInReservedsApi" class for call checkPrice endpoint

--- Backend/Agah/Agah/API's/ApiJobService.cs
+++ Backend/Agah/Agah/API's/ApiJobService.cs
@@ -1,4 +1,6 @@
-﻿using Datalayer.Repositories.IRepositories;
+﻿using Datalayer.Models;
+using Datalayer.Repositories.IRepositories;
+using Newtonsoft.Json;
 
 namespace Agah.API_s
 {
@@ -25,9 +27,38 @@ namespace Agah.API_s
             }
         }
 
-        public static async Task CallCheckPriceInReservedsApi(int userId)
+        public static async Task CallCheckPriceInReservedsApi()
         {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = "https://localhost:44314/api/PriceAlert/GetReserves";
 
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+                    string result = await response.Content.ReadAsStringAsync();
+                    var reservesList = JsonConvert.DeserializeObject<List<Reserve>>(result);
+
+                    string checkPriceUrl = "";
+
+
+                    foreach (var reserve in reservesList)
+                    {
+                        checkPriceUrl = $"https://localhost:44314/api/PriceAlert/CheckPriceInReserveds/{reserve.User_Id}";
+                        response = await client.GetAsync(checkPriceUrl);
+                        response.EnsureSuccessStatusCode();
+
+                        result = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine($"Checked price for User {reserve.User_Id}: {result}");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error calling CheckPriceInReserveds API: {ex.Message}");
+                }
+            }
         }
     }
 
