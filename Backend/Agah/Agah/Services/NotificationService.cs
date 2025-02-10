5889c99e903fb3bb5c8dc0651553421d29c95da3
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-10 20:14:00 +0330

    :construction: Backend | create "SendNotification" endpoint service

---
+++ Backend/Agah/Agah/Services/NotificationService.cs
@@ -0,0 +1,49 @@
+﻿using Agah.Controllers;
+using Agah.ViewModels;
+using Datalayer.Models;
+using Datalayer.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agah.Services
+{
+    public class NotificationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public NotificationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ResponseStatus> SendNotification(Notification_MessageOptions messageOptions)
+        {
+            try
+            {
+                var alarm = _unitOfWork.AlarmRepository.GetFirstOrDefault(u => u.EnglishName == messageOptions.NotificationType);
+
+                if (alarm == null || alarm.IsActive == false)
+                    return new ResponseStatus() { StatusCode = 400, StatusMessage = "آلارم انتخاب شده وجود ندارد یا درحال حاضر غیرفعال میباشد" };
+
+                switch (alarm.EnglishName)
+                {
+                    case "Alert":
+                        _unitOfWork.Notification_UserRepository.Add(new Notification_User()
+                        {
+                            UserId = messageOptions.User.Id,
+                            MessageSubject = messageOptions.MessageSubject,
+                            MessageContent = messageOptions.MessageContent,
+                        });
+                        await _unitOfWork.SaveAsync();
+                        return new ResponseStatus() { StatusCode = 200, StatusMessage = "عملیات با موفقیت انجام شد" };
+                    default:
+                        break;
+                }
+
+                return new ResponseStatus() { StatusCode = 200, StatusMessage = "عملیات با موفقیت انجام شد" };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseStatus() { StatusCode = 400, StatusMessage = $"عملیات با خطا مواجه شد\nخطا : {ex.Message}" };
+            }
+        }
+    }
+}
