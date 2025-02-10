5a4f65e49bde08bbd9a09d830cc984a6cd6356b9
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-10 20:15:27 +0330

    :construction: Backend | add "NotificationController" and "GetNotifications" endpoint

---
+++ Backend/Agah/Agah/Controllers/NotificationController.cs
@@ -0,0 +1,49 @@
+﻿using Datalayer.Models;
+using Datalayer.Repositories.IRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agah.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public NotificationController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet("GetNotifications/{userId}/{alarmType}")]
+        public async Task<IActionResult> GetNotifications(int userId, string alarmType)
+        {
+            try
+            {
+                switch (alarmType)
+                {
+                    case "Alert":
+                        var notificationsList = _unitOfWork.Notification_UserRepository.GetAllByFilter(u => u.UserId == userId, includeProperties: u => u.User);
+                        return Ok(notificationsList);
+                    default:
+                        break;
+                }
+
+                return Ok(new { message = "عملیات با موفقیت انجام شد" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"عملیات با خطا مواجه شد\nخطا : {ex.Message}" });
+            }
+        }
+    }
+
+    public class Notification_MessageOptions
+    {
+        public string MessageSubject { get; set; }
+        public string MessageContent { get; set; }
+        public string NotificationType { get; set; }
+        public User User { get; set; }
+    }
+}
