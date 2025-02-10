45d7703b038f410c4f2e5d6632c82bae4b9606aa
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-10 20:55:36 +0330

    :art: Frontend | make component for skeleton

--- Frontend/Agah/src/components/product_status_card_list.jsx
+++ Frontend/Agah/src/components/product_status_card_list.jsx
@@ -1,6 +1,7 @@
 ﻿import React, { useEffect, useState, useRef } from 'react';
 import ProductStatusCard from './product_status_card';
 import { getProductsLog } from './../services/api_GetProductsLog';
+import Skeleton from './skeleton';
 
 
 const ProductStatusCardList = () => {
@@ -47,15 +48,8 @@ const ProductStatusCardList = () => {
                             />
                         ))
                     ) : (
-                        <div role="status" class="max-w-sm animate-pulse">
-                            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-                            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-                            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-                            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-                            <span class="sr-only">Loading...</span>
-                        </div>)}
+                        <Skeleton />
+                    )}
                 </div>
             </div>
         </>
