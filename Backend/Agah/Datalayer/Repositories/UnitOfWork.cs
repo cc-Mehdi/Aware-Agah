6ff9cffdc2f0dfd3b2c31ebff36c519416e5b628
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-10 20:12:27 +0330

    :bug: Database | add IReserveRepository to unitofwork

--- Backend/Agah/Datalayer/Repositories/UnitOfWork.cs
+++ Backend/Agah/Datalayer/Repositories/UnitOfWork.cs
@@ -11,16 +11,16 @@ namespace Datalayer.Repositories
             _db = db;
             UserRepository = new UserRepository(_db);
             ProductRepository = new ProductRepository(_db);
-            AlarmRepository = new AlarmRepository(_db);
             ProductLogRepository = new ProductLogRepository(_db);
+            AlarmRepository = new AlarmRepository(_db);
             ReserveRepository = new ReserveRepository(_db);
             Notification_UserRepository = new Notification_UserRepository(_db);
         }
 
         public IUserRepository UserRepository{ get; set; }
         public IProductRepository ProductRepository { get; set; }
-        public IAlarmRepository AlarmRepository { get; set; }
         public IProductLogRepository ProductLogRepository { get; set; }
+        public IAlarmRepository AlarmRepository { get; set; }
         public IReserveRepository ReserveRepository { get; set; }
         public INotification_UserRepository Notification_UserRepository { get; set; }
 
