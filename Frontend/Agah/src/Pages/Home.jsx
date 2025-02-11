269a249bc4caeccfc160fb9ca7130a45cb16a967
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-11 22:34:54 +0330

    :white_check_mark: Frontend | add "NotificationSidebar" component to HomePage

--- Frontend/Agah/src/Pages/Home.jsx
+++ Frontend/Agah/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProductStatusList from './../components/product_status_card_list'
-import SetPriceRangeReservation from '../components/setPriceRangeReservation_section'
+import SetPriceRangeReservation from './../components/setPriceRangeReservation_section'
+import NotificationSidebar from './../components/notification_sidebar'
 
 const Home = () => {
     return (
         <>
+            <NotificationSidebar />
             <ProductStatusList />
             <SetPriceRangeReservation />
         </>
