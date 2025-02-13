8d9738805a66fc2466ab49b5b71e72969e05cfe6
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-13 13:41:48 +0330

    :lipstick: Frontend | change style for not active alarms in "radiobutton_platform_selector" component

--- Frontend/Agah/src/components/radiobutton_platform_selector.jsx
+++ Frontend/Agah/src/components/radiobutton_platform_selector.jsx
@@ -22,11 +22,17 @@ const RadioButtonPlatformSelector = ({ inputName, dataSource, selectedValue, onC
                         checked={selectedValue === platform.id.toString()} // Controls the selection
                         onChange={(e) => onChange(e.target.value)} // Updates the parent state
                         required
+                        disabled={!platform.isActive}
                     />
                     <label
                         htmlFor={platform.id}
-                        className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                        className={`inline-flex items-center relative justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer
+                             dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600
+                              peer-checked:text-blue-600 dark:text-gray-400 dark:bg-gray-800 ${platform.isActive && ('dark:hover:bg-gray-700 hover:bg-gray-100 dark:hover:text-gray-300 hover:text-gray-600')}`}
                     >
+                        {!platform.isActive && (
+                            <div className="backdrop-blur-xs w-full h-full absolute z-30 top-0 right-0"></div>
+                        )}
                         <div className="block">
                             <div className="w-full text-lg font-semibold">{platform.persianName}</div>
                             <div className="w-full">{platform.shortDescription}</div>
