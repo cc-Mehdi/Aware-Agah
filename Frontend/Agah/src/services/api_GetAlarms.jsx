c63e649b6aa175a926fe98c2681207b08d1b8024
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-13 13:35:52 +0330

    :alien: Frontend | create "api_GetAlarms" component

---
+++ Frontend/Agah/src/services/api_GetAlarms.jsx
@@ -0,0 +1,17 @@
+export const getAlarms = async () => {
+    try {
+        const response = await fetch('https://localhost:44314/api/Alarms/GetAlarms', {
+            method: 'Get',
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.result;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
