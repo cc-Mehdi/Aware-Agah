7e7a324626f1c78c868bbadd5fee78afa791b223
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-13 22:40:44 +0330

    :lipstick: Frontend | make different between readed and unreaded notifications in "notification_card" component

--- Frontend/Agah/src/components/notification_card.jsx
+++ Frontend/Agah/src/components/notification_card.jsx
@@ -6,9 +6,12 @@ const notification_card = (notificationItem) => {
     let notification = notificationItem.notificationItem;
 
     return (
-        <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <span className="font-normal text-gray-700 dark:text-gray-400" dir='ltr'>
+        <div className={`block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 ${!notification.isRead && 'dark:hover:bg-gray-700 hover:bg-gray-100 cursor-pointer'}`}>
+            <span className="font-normal text-gray-700 dark:text-gray-400 flex flex-row-reverse justify-between" dir='ltr'>
                 {moment(notification.createdAt).format("jYYYY-jMM-jDD | HH:mm:ss")} {/* convert to persian date */}
+                {notification.isRead && (
+                    <i class="fa-solid fa-check-double me-2" aria-hidden="true"></i>
+                )}
             </span>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{notification.messageSubject}</h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">{notification.messageContent}</p>
