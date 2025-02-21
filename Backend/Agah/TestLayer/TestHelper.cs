b9407f57c12098767c3803904d25773247452a43
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-21 23:01:11 +0330

    :construction: Testing | Improve methods exist in "TestHelper" file

--- Backend/Agah/TestLayer/TestHelper.cs
+++ Backend/Agah/TestLayer/TestHelper.cs
@@ -4,9 +4,11 @@ using Datalayer.Repositories.IRepositories;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace TestLayer
@@ -21,10 +23,19 @@ namespace TestLayer
             )).ReturnsAsync(fakeNotifications);
         }
 
-        public static void SetupUserRepo(Mock<IUnitOfWork> mockUnitOfWork, User user)
+        public static void SetupRepository<T>(Mock<IUnitOfWork> mockUnitOfWork, T entity) where T : class
         {
-            mockUnitOfWork.Setup(u => u.UserRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<User, bool>>>()))
-                          .ReturnsAsync(user);
+            var repoMock = new Mock<IRepository<T>>();
+            repoMock.Setup(r => r.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<T, bool>>>())).ReturnsAsync(entity);
+
+            // دریافت نام صحیح Property مربوط به ریپازیتوری
+            var propertyInfo = typeof(IUnitOfWork).GetProperties()
+                .FirstOrDefault(p => p.PropertyType == typeof(IRepository<T>));
+
+            if (propertyInfo != null)
+            {
+                mockUnitOfWork.Setup(u => propertyInfo.GetValue(u)).Returns(repoMock.Object);
+            }
         }
 
         public static void SetupNotificationUpdate(Mock<IUnitOfWork> mockUnitOfWork)
@@ -37,10 +48,17 @@ namespace TestLayer
         public static void AssertBadRequestWithMessage(IActionResult result, string expectedMessage)
         {
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
-            var errorResponse = JsonConvert.DeserializeObject<ResponseStatus>(JsonConvert.SerializeObject(badRequestResult.Value));
-            Assert.NotNull(errorResponse);
-            Assert.Equal(expectedMessage, errorResponse.StatusMessage);
+            var jsonObject = JsonConvert.DeserializeObject<ResponseStatus>(JsonConvert.SerializeObject(badRequestResult.Value));
+            Assert.Equal(expectedMessage, jsonObject.StatusMessage?.ToString());
         }
+
+        public static void AssertOkWithMessage(IActionResult result, string expectedMessage)
+        {
+            var badRequestResult = Assert.IsType<OkObjectResult>(result);
+            var jsonObject = JsonConvert.DeserializeObject<ResponseStatus>(JsonConvert.SerializeObject(badRequestResult.Value));
+            Assert.Equal(expectedMessage, jsonObject.StatusMessage?.ToString());
+        }
+
         public static List<T> ExtractListFromResult<T>(OkObjectResult okResult)
         {
             Assert.NotNull(okResult.Value);
@@ -51,6 +69,20 @@ namespace TestLayer
             }
 
             throw new InvalidCastException($"Expected value of type IEnumerable<{typeof(T).Name}>, but got {okResult.Value.GetType().Name}.");
+        }
+
+        public static List<T> ExtractListFromResultWithKey<T>(OkObjectResult okResult, string key)
+        {
+            Assert.NotNull(okResult.Value);
+
+            // Convert the response object to a JObject
+            var jsonObject = JObject.FromObject(okResult.Value);
+
+            // Extract and deserialize the "result" property
+            var dataArray = jsonObject[key]?.ToObject<List<T>>();
+
+            Assert.NotNull(dataArray);
+            return dataArray;
         }
     }
 }
