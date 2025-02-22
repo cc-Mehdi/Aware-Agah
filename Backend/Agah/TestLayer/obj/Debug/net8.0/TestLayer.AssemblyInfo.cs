ba9c8d716a1c9c2491b6c45f0281e04d6b3a4f45
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-22 11:16:56 +0330

    :white_check_mark: Testing | Update Tests

--- Backend/Agah/TestLayer/obj/Debug/net8.0/TestLayer.AssemblyInfo.cs
+++ Backend/Agah/TestLayer/obj/Debug/net8.0/TestLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77528d76f41f34445145ab103e748d0a58eb8e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12862fe3cd07ce8e729e80b16ff5a874d7f9d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
