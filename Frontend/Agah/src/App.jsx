ba2b19073f43af1ad87411c369eae344e3932d82
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-02-25 23:34:30 +0330

    :lipstick: Frontend | Change font to "Vazirmatn"

--- Frontend/Agah/src/App.jsx
+++ Frontend/Agah/src/App.jsx
@@ -1,8 +1,16 @@
 import Home from './Pages/Home'
 import ErrorHandler from "./components/ErrorHandler";
+import { useEffect } from 'react';
+
 
 function App() {
-
+useEffect(() => {
+        const link = document.createElement('link');
+        link.href = 'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;700&display=swap';
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
+        document.body.style.fontFamily = 'Vazirmatn, sans-serif';
+    }, []);
     return (
         <>
             <ErrorHandler />
