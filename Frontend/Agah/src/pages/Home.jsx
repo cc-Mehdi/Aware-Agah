a0c9c662136a7d124d2bbddb0ed76175f06f6c84
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-03-06 01:58:48 +0330

    :lock: Frontend | Fix security issue
    this security fix, improve code performance

---
+++ Frontend/Agah/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { React, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProductStatusList from './../components/product_status_card_list';
+import SetPriceRangeReservation from './../components/setPriceRangeReservation_section';
+import NotificationSidebar from './../components/notification_sidebar';
+import { validateToken } from './../services/api_ValidateToken';
+
+const Home = () => {
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(null); // `null` means "still checking"
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const isValid = await validateToken();
+            if (!isValid) {
+                localStorage.removeItem("token");
+                navigate("/login");
+            } else {
+                setIsAuthenticated(true);
+            }
+        };
+
+        checkAuth();
+    }, []);
+
+    if (isAuthenticated === null) {
+        <div className="flex items-center justify-center h-screen text-white font-bolder text-3xl">در حال احراز هویت شما ...</div>
+    }
+
+    return (
+        <>
+            <NotificationSidebar />
+            <ProductStatusList />
+            <SetPriceRangeReservation />
+        </>
+    );
+};
+
+export default Home;
