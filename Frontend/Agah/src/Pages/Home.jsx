c1d78c1e285c59feca26e44b85c1b655caa5ccbf
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-03-06 01:16:27 +0330

    :lock: Frontend | Implement ValidationToken in Home Page

--- Frontend/Agah/src/Pages/Home.jsx
+++ Frontend/Agah/src/Pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import ProductStatusList from './../components/product_status_card_list'
 import SetPriceRangeReservation from './../components/setPriceRangeReservation_section'
 import NotificationSidebar from './../components/notification_sidebar'
+import { validateToken } from './../services/api_ValidateToken'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        validateToken().then((isValid) => {
+            if (!isValid) {
+                localStorage.removeItem("token");
+                navigate("/login");
+            }
+        });
+    }, []);
+
     return (
         <>
             <NotificationSidebar />
