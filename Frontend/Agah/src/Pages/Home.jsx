5ef685d91d62a342fe203bfe5116fc7269e890e1
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-03-06 23:13:02 +0330

    Update Home Page

--- Frontend/Agah/src/Pages/Home.jsx
+++ Frontend/Agah/src/Pages/Home.jsx
@@ -1,22 +1,31 @@
-import { React, useEffect } from 'react'
-import ProductStatusList from './../components/product_status_card_list'
-import SetPriceRangeReservation from './../components/setPriceRangeReservation_section'
-import NotificationSidebar from './../components/notification_sidebar'
-import { validateToken } from './../services/api_ValidateToken'
-import { useNavigate } from 'react-router-dom'
+import { React, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProductStatusList from './../components/product_status_card_list';
+import SetPriceRangeReservation from './../components/setPriceRangeReservation_section';
+import NotificationSidebar from './../components/notification_sidebar';
+import { validateToken } from './../services/api_ValidateToken';
 
 const Home = () => {
-
     const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(null); // `null` means "still checking"
 
     useEffect(() => {
-        validateToken().then((isValid) => {
+        const checkAuth = async () => {
+            const isValid = await validateToken();
             if (!isValid) {
                 localStorage.removeItem("token");
                 navigate("/login");
+            } else {
+                setIsAuthenticated(true);
             }
-        });
+        };
+
+        checkAuth();
     }, []);
+
+    if (isAuthenticated === null) {
+        <div className="flex items-center justify-center h-screen text-white font-bolder text-3xl">در حال احراز هویت شما ...</div>
+    }
 
     return (
         <>
@@ -24,7 +33,7 @@ const Home = () => {
             <ProductStatusList />
             <SetPriceRangeReservation />
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
