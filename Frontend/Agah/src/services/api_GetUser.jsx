12ba8c40d553794fc758b323e3dcba8e7c9527a4
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-03-08 22:16:01 +0330

    :alien: Frontend | Create api component for Getting User Information

---
+++ Frontend/Agah/src/services/api_GetUser.jsx
@@ -0,0 +1,21 @@
+import { API_BASE_URL, getToken } from "./../../config.js";
+
+export const getUser = async () => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/Users/GetUser`, {
+            method: 'Get',
+            headers: { authorization: `Bearer ${getToken()}` },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+
+        const data = await response.json();
+        return data.result;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
