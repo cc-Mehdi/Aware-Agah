3d0cedc880db060d89f11153b8c0c484425fd0a3
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-03-09 23:51:21 +0330

    :art: Backend | Add "Auth" utility file

---
+++ Backend/Agah/Agah/Utility/Auth.cs
@@ -0,0 +1,26 @@
+﻿namespace Agah.Utility
+{
+    public static class Auth
+    {
+        public static bool IsUserExist(HttpContext httpContext)
+        {
+            // Extract email from the JWT token (assuming it's stored in the claims)
+            var userEmail = httpContext.User.Identities.First().Name;
+
+            if (string.IsNullOrEmpty(userEmail))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string GetUserEmail(HttpContext httpContext)
+        {
+            // Extract email from the JWT token (assuming it's stored in the claims)
+            var userEmail = httpContext.User?.Identities?.First()?.Name ?? "";
+
+            return userEmail;
+        }
+    }
+}
