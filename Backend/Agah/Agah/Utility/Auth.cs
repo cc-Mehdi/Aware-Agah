224a227ff029bc14b71c16e5ac347c5ea4386d0d
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-04-17 11:47:04 +0330

    :art: Backend | add GetLoggedInUserAsync function for getting user info

--- Backend/Agah/Agah/Utility/Auth.cs
+++ Backend/Agah/Agah/Utility/Auth.cs
@@ -1,4 +1,7 @@
-﻿namespace Agah.Utility
+﻿using Datalayer.Models;
+using Datalayer.Repositories.IRepositories;
+
+namespace Agah.Utility
 {
     public static class Auth
     {
@@ -21,6 +24,25 @@
             var userEmail = httpContext.User?.Identities?.First()?.Name ?? "";
 
             return userEmail;
+        }
+
+        public static async Task<User> GetLoggedInUserAsync(HttpContext httpContext, IUnitOfWork _unitOfWork)
+        {
+            if (!Auth.IsUserExist(httpContext))
+                return null;
+
+            // Fetch user from the database using the email
+            User user = await _unitOfWork.UserRepository.GetFirstOrDefaultAsync(u => u.Email == Auth.GetUserEmail(httpContext));
+
+            if (user == null)
+                return null;
+
+            return user;
+        }
+
+        public static string GenerateRandomToken()
+        {
+            return new Random().Next(100000, 999999).ToString(); // کد ۶ رقمی
         }
     }
 }
