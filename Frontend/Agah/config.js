8fbb478be751555f958f94f013a87b52f1455bb2
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-04-17 13:37:33 +0330

    :construction: Frontend | dynamicly changing api_base_url
    from .env file can change api_base_url

--- Frontend/Agah/config.js
+++ Frontend/Agah/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://192.168.2.137:5001/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Function to get the latest token
 export const getToken = () => localStorage.getItem("token");
