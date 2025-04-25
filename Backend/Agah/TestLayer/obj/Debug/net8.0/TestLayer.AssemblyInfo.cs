abb6d5d550e23a21b4e2ab8f31f14d0ae502a27b
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-04-25 12:03:44 +0330

    🚧 VS updated base files

--- Backend/Agah/TestLayer/obj/Debug/net8.0/TestLayer.AssemblyInfo.cs
+++ Backend/Agah/TestLayer/obj/Debug/net8.0/TestLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d699d449ed3dbc818a7f6e0fba5cc4b1228c37d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5eb3b9368a1c0724825dea2f005c599947fcb4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
