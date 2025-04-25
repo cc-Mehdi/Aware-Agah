fc207cb5c64552b86408938926ac111384990f36
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2025-04-25 12:01:48 +0330

    :bug: Frontend | change host address in vite config
    for getting access to site throw the network

--- Frontend/Agah/vite.config.js
+++ Frontend/Agah/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 5173, // Optional: Specify the port (default is 5173)
   },
 });
